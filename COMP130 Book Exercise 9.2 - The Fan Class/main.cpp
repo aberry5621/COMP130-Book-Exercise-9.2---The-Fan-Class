c40bbe42e7487568f2ca1276dd8d8cf77f543807
aberry5621 <aberry5621@gmail.com>
2016-10-26 10:20:12 -0700

    Initial Commit

---
+++ COMP130 Book Exercise 9.2 - The Fan Class/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP130 Book Exercise 9.2 - The Fan Class
+//
+//  Created by ax on 10/26/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
